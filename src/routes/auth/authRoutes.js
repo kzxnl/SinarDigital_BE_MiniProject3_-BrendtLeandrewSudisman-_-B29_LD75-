4d7ef5afe2974f63d8eadeea1b2340f22f57c001
kzxnl <7brendt@gmail.com>
2025-12-17 21:58:13 +0700

    Create authRoutes.js

---
+++ src/routes/auth/authRoutes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const ctrl = require('../../controllers/auth/authController');
+router.post('/register', ctrl.register);
+router.post('/login', ctrl.login);
+router.post('/logout', ctrl.logout);
+module.exports = router;
